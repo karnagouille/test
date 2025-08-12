7439fe724fd17bdcea955779ddcf5850791041c7
karnage <gonzalesalexis999@gmail.com>
2025-08-12 16:11:48 +0200

    'add-remove'

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Post; 
+use App\Form\PostType;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PostController extends AbstractController
 {
-    #[Route('/post', name: 'post', methods:['GET'])]
+    #[Route('/post', name: 'post_index', methods:['GET'])]
     public function index(PostRepository $postRepo): Response
     {
         $posts = $postRepo->findAll();
@@ -18,5 +22,50 @@ final class PostController extends AbstractController
             'posts'=> $posts,
         ]);
 
+        
     }
+    
+      // Route pour créer un nouveau post
+    #[Route('/post/create', name: 'post_create', methods:['GET','POST'])]
+
+    public function create(Request $request, EntityManagerInterface $em):Response
+
+{    // Création d'un nouvel objet Post
+    $post = new Post();
+
+
+    // Création du formulaire basé sur le type PostType
+    $form = $this->createForm(PostType::class,$post);
+    $form->handleRequest($request);
+
+    // Si le formulaire est soumis et valide
+    if($form->isSubmitted()&& $form->isValid())
+    {   //envoie du formulaire
+        $em->persist($post);
+        $em->flush();
+        return $this->redirectToRoute('post_index');
+    }
+     // Redirection vers la page listant tous les posts
+    return $this->render('post/create.html.twig',[
+        'form'=>$form->createView()
+    ]);
+}
+
+#[Route('post/{id}/remove',name: 'post_remove', methods:['POST'])]
+public function remove(EntityManagerInterface $em, Post $post, Request $request):Response
+{
+
+
+if ($this->isCsrfTokenValid('delete'.$post->getId(),$request->request->get('_token')))
+{
+    $em->remove($post);
+    $em->flush();
+}
+return $this->redirectToRoute('post_index');
+    
+
+    }
+
+
+
 }
